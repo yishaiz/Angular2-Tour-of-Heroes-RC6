62c489dd86f70eb3500d97a707a4e7db71adbfe4
yishaiz <yishai5555@gmail.com>
2016-09-07 23:19:15 +0300

    b6

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,6 +3,21 @@ import {Component} from '@angular/core';
 import {Hero} from './Heroes/hero';
 import {HeroDetailComponent} from './Heroes/hero-detail.component';
 
+
+const HEROES: Hero[] = [
+  {id: 11, name: 'Mr. Nice'},
+  {id: 12, name: 'Narco'},
+  {id: 13, name: 'Bombasto'},
+  {id: 14, name: 'Celeritas'},
+  {id: 15, name: 'Magneta'},
+  {id: 16, name: 'RubberMan'},
+  {id: 17, name: 'Dynama'},
+  {id: 18, name: 'Dr IQ'},
+  {id: 19, name: 'Magma'},
+  {id: 20, name: 'Tornado'}
+];
+
+
 @Component({
   selector: 'my-app',
   // directives:[HeroDetailComponent],
@@ -96,36 +111,22 @@ import {HeroDetailComponent} from './Heroes/hero-detail.component';
 export class AppComponent {
   title = 'Tour of Heroes';
 
-  selectedHero:Hero;
+  selectedHero: Hero;
 
-
-  //const
-  heroes:Hero[] = [
-    {id: 11, name: 'Mr. Nice'},
-    {id: 12, name: 'Narco'},
-    {id: 13, name: 'Bombasto'},
-    {id: 14, name: 'Celeritas'},
-    {id: 15, name: 'Magneta'},
-    {id: 16, name: 'RubberMan'},
-    {id: 17, name: 'Dynama'},
-    {id: 18, name: 'Dr IQ'},
-    {id: 19, name: 'Magma'},
-    {id: 20, name: 'Tornado'}
-  ];
-
+  heroes: Hero[] = HEROES;
   // public heroes=HEROS;
 
-  hero:Hero = {
+  hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
 
-  onSelect(hero:Hero) {
+  onSelect(hero: Hero) {
     this.selectedHero = hero;
     console.log(this.selectedHero);
   }
 
-  trackByHeroes(index: number, hero: Hero){
+  trackByHeroes(index: number, hero: Hero) {
     return hero.id;
   }
 
